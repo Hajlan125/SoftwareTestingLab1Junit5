ea32b01c7f2fe594eabe74ec1f313ce207fedab6
Гараев Адель <hajlan.adel@mail.ru>
2021-10-06 17:49:43 +0300

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+    public static LoginPage loginPage;
+    public static ProfilePage profilePage;
+    public static MailPage mailPage;
+    public static WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        mailPage = new MailPage(driver);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("loginpage"));
+    }
+
+    @Test
+    public void loginTest() {
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        loginPage.clickLoginBtn();
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        loginPage.clickLoginBtn();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String user = profilePage.getUserName();
+        assertEquals(ConfProperties.getProperty("login"),user);
+        profilePage.enterMyServices();
+        driver.manage().timeouts().setScriptTimeout(2, TimeUnit.SECONDS);
+        profilePage.enterMail();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+//        profilePage.entryMenu();
+//        profilePage.userLogout();
+        mailPage.entryUserMenu();
+        mailPage.usrLogout();
+        driver.quit();
+    }
+}
