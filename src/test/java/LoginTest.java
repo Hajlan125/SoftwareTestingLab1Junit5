ef99685d229f8b5827eaf34c87633aaeda9c1de7
Гараев Адель <hajlan.adel@mail.ru>
2021-10-07 18:42:07 +0300

    Удален ненужный импорт

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
